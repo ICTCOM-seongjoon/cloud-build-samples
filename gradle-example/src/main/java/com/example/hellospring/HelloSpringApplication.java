494e5ca2da803b9613f0ce2de9dfde6b528e8a46
ICTCOM-seongjoon <sjkim@ictcom.co.kr>
2022-10-06 08:13:04 +0900

    helloworld3

--- gradle-example/src/main/java/com/example/hellospring/HelloSpringApplication.java
+++ gradle-example/src/main/java/com/example/hellospring/HelloSpringApplication.java
@@ -39,7 +39,7 @@ public class HelloSpringApplication implements CommandLineRunner {
 	}
 
 	public final static String getBanner(String mode) {
-		final String greeting = "Hello World2!";
+		final String greeting = "Hello World2s!";
 
 		final String logoBW = 
 		"                                                                                \n"+
